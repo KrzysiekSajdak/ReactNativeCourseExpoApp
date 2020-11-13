f74096237863629333f8434c882675d4ba07da78
KrzysiekSajdak <krzysztofsajdak@outlook.com>
2020-11-13 10:30:59 +0000

    learning in progress

--- App.js
+++ App.js
@@ -1,36 +1,73 @@
+// 2:35:14
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, SafeAreaView, ScrollView, StyleSheet, Text, TextInput, View } from 'react-native';
 
 import Todo from './Todo'
 
 const App = () => {
 
-  const [input, setInput] = useState('');
-  const [todos, setTodos] = useState([]);
+  const [description, setDescription] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [gigs, setGigs] = useState([
+    {
+      description: 'Freelance job with Qazi',
+      amount: 499.99
+    },
+    {
+      description: 'Freelance job with Qazi',
+      amount: 499.99
+    }
+  ]);
 
-
-  const addTodo = () => {
-    setTodos([input, ...todos]);
-    setInput('');
+  const addGig = () => {
+    setGigs([...gigs, {
+      description: description,
+      amount: amount
+    }]);
+    setDescription('');
+    setAmount('')
   }
 
+  useEffect(() => {
+    setTotal(gigs.reduce((total, gig) => total + Number(gig.amount), 0))
+  }, [gigs])
+
   return (
-    <SafeAreaView>
+    <SafeAreaView style={styles.mainStyle}>
       <View>
         <Text style={styles.titleText}>Todo App</Text>
       </View>
-      <ScrollView style={styles.todoList}>
-        {todos.map(todo => (<Todo title={todo}></Todo>))}
-      </ScrollView>
+      <Text>Total income: ${total}</Text>
       <TextInput
-        value={input}
-        onChangeText={text => setInput(text)}
+        value={description}
+        keyboardType='default'
+        placeholder='Enter a description'
+        onChangeText={text => setDescription(text)}
+        style={styles.todoInput} />
+      <TextInput
+        value={amount}
+        keyboardType='numeric'
+        placeholder='Enter the amount you made'
+        onChangeText={text => setAmount(text)}
         style={styles.todoInput} />
       <Button
+        title='Add gig'
+        disabled={!amount || !description}
+        onPress={addGig} />
+
+      {gigs.map(gig => (
+        <View>
+          <Text>{gig.description}</Text>
+          <Text>{gig.amount}</Text>
+        </View>
+
+      ))}
+      {/* <Button
         title='ADD TODO'
         style={styles.todoButton}
-        onPress={addTodo} />
+        onPress={addTodo} /> */}
     </SafeAreaView>
 
   );
@@ -39,14 +76,19 @@ const App = () => {
 export default App;
 
 const styles = StyleSheet.create({
+  mainStyle: {
+    margin: 15
+  },
   titleText: {
-    fontSize: 40,
+    fontSize: 30,
+    paddingBottom: 15
   },
   todoInput: {
     fontSize: 20,
-    height: 40,
+    height: 30,
     margin: 10,
-    borderRadius: 15,
+    padding: 10,
+    borderRadius: 10,
     borderWidth: 1,
     borderColor: 'black'
   },
